bb9fabaaed966d722a7f6d68cd07852fe7dfe01e
Sam Ottenhoff <ottenhoff@longsight.com>
2026-06-19 19:50:58 -0400

    Avoid TypeScript runtime import in platform worker

--- src/app_writer/platform_worker.ts
+++ src/app_writer/platform_worker.ts
@@ -1,13 +1,15 @@
 import { createCloudflareBrowserAppPackagePreviewer } from './cloudflare_browser_preview.ts';
 import { createAppWriterPlatformServicesHandler } from './platform_services.ts';
 import { createUnavailableAppPackagePreviewer } from './preview.ts';
-import { createTypeScriptAppPackageSourceCompiler } from './typescript_source_compiler.ts';
+import { createUnavailableAppPackageSourceCompiler } from './source_compiler.ts';
 
 interface PlatformWorkerEnv {
   BROWSER?: unknown;
 }
 
-const sourceCompiler = createTypeScriptAppPackageSourceCompiler();
+const sourceCompiler = createUnavailableAppPackageSourceCompiler(
+  'Lantern TypeScript app source compilation is not available in the Cloudflare platform service.',
+);
 
 export default {
   fetch(request: Request, env: PlatformWorkerEnv): Promise<Response> {
